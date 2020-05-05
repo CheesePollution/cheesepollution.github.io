519ebbd3da6480055f70d1376b482b9e3dd4067c
Zion Chen <leafymeap@gmail.com>
2020-05-05 20:39:46 +0800

    Made buttons more responsive to web design

--- JSSheets/RandomTicker.js
+++ JSSheets/RandomTicker.js
@@ -14,6 +14,7 @@ var shuffle = function(array) {
 }
 
 window.addEventListener("load", randomizeTicker, true);
+window.addEventListener("load", fixBtns, true);
 
 var tickerQuestions = ["You are a business man from the UN with a reputation of a homeless man... Yeah so save Earth from pollution, good luck", "Don't die from corona or global warming >:).", "FREEZE YOUR BRAINN. Shatter your skull, fight pain with more pain...", "flkdjasluisenifnidsajiofjdklsajk;.", "'Oil Company sued for 'misleading people with adverts' by City X", "Conspiracy Theorist: 'Those Environmentalists are killing Elephants!'", "lol", "Country files bankruptcy for 'oil industry not profitable'", "Breaking News: Earth is Flat. And The Sun is star shaped.", "NASA: Flat Earth Conspiracy has taken over a News Network", "Breaking News: News Network files bankruptcy: 'No one watches TV anymore'", "Flat Earth Society admits false conspiracy online, claiming The Earth is a globe supported on the back of a giant universe turtle", "I'm gay", "Thank", "monkey's be like ooo ooo ah ah", "gaganajeet", "5G towers confirmed to not cause Covid-19, instead, they make ducks horny", "Pick a place to die where it's high and dry"];
 
@@ -35,6 +36,12 @@ function randomizeTicker() {
 
   var cls = document.getElementsByClassName("ticker-move");
   cls.inbetween.style.animationDuration = animationDurationTime + "s";
+}
+
+function fixBtns() {
+  document.getElementById("opt1").style.width = width*0.5 + "px";
+  document.getElementById("opt2").style.width = width*0.5 + "px";
+  document.getElementById("opt3").style.width = width*0.5 + "px";
 }
 
 function hover(element) {
